92d2281e7ff59a61fd71694a838151f624809fe5
Jonatan Gomez <jgomezpe@unal.edu.co>
2020-01-23 22:16:39 -0500

    Add: Demo classes for hazarda

--- hazarda-demo/src/demo/random/RandBitDemo.java
+++ hazarda-demo/src/demo/random/RandBitDemo.java
@@ -10,9 +10,7 @@ public class RandBitDemo {
 		int n = 10;
 		// Generating an array of ten random values
 		boolean[] x = g.generate(n);
-		for( int i=0; i<n; i++ ){
-			System.out.println( x[i] );
-		}
+		for( int i=0; i<n; i++ ) System.out.println( x[i] );
 		System.out.println("****************");
 		// Generating ten random values
 		for( int i=0; i<n; i++ ){
